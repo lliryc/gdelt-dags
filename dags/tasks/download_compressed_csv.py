1480ce20633cec86007186091a6148e2559bbecd
Kirill Chirkunov <kirill.chirkunov@gmail.com>
2024-09-10 23:27:45 +0400

    fixed raw gdelt table

--- dags/tasks/download_compressed_csv.py
+++ dags/tasks/download_compressed_csv.py
@@ -1,8 +1,11 @@
 import requests
 import io
 import zipfile
+import pandas as pd
+from sqlalchemy import create_engine
+import io
 
-def download_zip_content(url):
+def write_raw_gkg_content_to_db(url):
     # Define headers
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
@@ -11,7 +14,8 @@ def download_zip_content(url):
 
     # Send a GET request to the URL with headers
     response = requests.get(url, headers=headers)
-        # Check if the request was successful
+    
+    # Check if the request was successful
     if response.status_code == 200:
         # Create a BytesIO object from the content
         zip_content = io.BytesIO(response.content)
@@ -20,19 +24,38 @@ def download_zip_content(url):
         with zipfile.ZipFile(zip_content, 'r') as zip_ref:
             # Extract all contents to memory
             extracted_content = {name: zip_ref.read(name) for name in zip_ref.namelist()}
-        
-        return extracted_content
+            
+        for _, csv_content in extracted_content.items():
+            # Create a list of column names from the SQL file
+            columns = [
+                'record_id', 'date_label', 'source_collection_id', 'source_common_name',
+                'document_identifier', 'counts', 'counts_v2', 'themes', 'themes_enhanced',
+                'locations', 'locations_enhanced', 'persons', 'persons_enhanced',
+                'organizations', 'organizations_enhanced', 'tone', 'dates_enhanced',
+                'gcam', 'sharing_image', 'related_images', 'social_image_embeds', 'social_video_embeds',
+                'quotations', 'all_names', 'amounts', 'translation_info', 'extras_xml'
+            ]
+
+            # Read the CSV content into a pandas DataFrame
+            df = pd.read_csv(io.StringIO(csv_content.decode("iso-8859-1")), sep='\t', header=None, names=columns)
+          
+            # Create the SQLAlchemy engine
+            engine = create_engine('postgresql+psycopg2://airflow:airflow@127.0.0.1/gdelt')
+            
+            connection = engine.connect()
+            
+            # Save the DataFrame to the PostgreSQL table
+            df.to_sql('gkg_raw', connection, schema='raw', if_exists='append', index=False)
+            
+            connection.close()
+
+        return True
+    
     else:
         print(f"Failed to download the file. Status code: {response.status_code}")
-        return None
+        return False
 
-# Example usage
-url = "http://data.gdeltproject.org/gdeltv2/20150218230000.export.CSV.zip"
-content = download_zip_content(url)
+res = write_raw_gkg_content_to_db("http://data.gdeltproject.org/gdeltv2/20150218230000.gkg.csv.zip") 
 
-if content:
-    for filename, file_content in content.items():
-        print(f"File: {filename}, Size: {len(file_content)} bytes")
-        with open(filename, 'wb') as f:
-            f.write(file_content)
-        
+print(res)       
+
